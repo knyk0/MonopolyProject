c9d4e83ddd130b1d41255b37e7ca0c9224104f8a
ndoughty4 <noradoughty@gmail.com>
2022-10-19 12:45:24 -0500

    first outline game class

--- Game.java
+++ Game.java
@@ -1,3 +1,38 @@
 public class Game {
 
+    CircularLinkedList players = new CircularLinkedList();
+    Player currentPlayer = players.first;
+    CircularLinkedList tiles = new CircularLinkedList();
+
+    public Game(){
+
+    }
+
+    public void printBoard(){
+
+    }
+    public void trade(){
+
+    }
+    public void upgrade(){
+
+    }
+    public void turn(){
+
+    }
+    public void buyProperty(){
+
+    }
+    public void payPayment(){
+
+    }
+    public void payTax(){
+
+    }
+    public void goToJail(){
+
+    }
+    public void getOutOfJail(){
+
+    }
 }
