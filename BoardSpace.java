b164af2bd536a2ce482d5106b628040993e5afbf
knyk0 <nicolaseaviles@gmail.com>
2022-10-20 09:29:21 -0500

    boardspace variables

--- BoardSpace.java
+++ BoardSpace.java
@@ -1,5 +1,12 @@
 public class BoardSpace {
 
+    Player owner;
+    int price;
+    int payment;
+    int housePrice;
+    int hotelPrice;
+    String propertyName;
+
     public enum tileType{Chance, CommunityChest, Railroad, FreeParking, Jail, GoToJail, Go, LuxuryTax, IncomeTax, Utilities, Red, Orange, Yellow, Green, Blue, lightBlue, Brown, Pink}
                             //0         1           2           3        4        5      6      7           8         9       10     11      12     13    14        15      16    17
 }
