c4336fe1d846c06a849f55cf3d2ab75c171154f6
ndoughty4 <noradoughty@gmail.com>
2022-10-24 09:58:57 -0500

    first outline game class more stuff

--- BoardSpace.java
+++ BoardSpace.java
@@ -6,7 +6,19 @@ public class BoardSpace {
     int housePrice;
     int hotelPrice;
     String propertyName;
+    String printName;
+    tileType thisTileType;
 
     public enum tileType{Chance, CommunityChest, Railroad, FreeParking, Jail, GoToJail, Go, LuxuryTax, IncomeTax, Utilities, Red, Orange, Yellow, Green, Blue, lightBlue, Brown, Pink}
                             //0         1           2           3        4        5      6      7           8         9       10     11      12     13    14        15      16    17
+
+    public BoardSpace(int price, int payment, int housePrice, int hotelPrice, String propertyName, String printName, tileType thisTileType){
+        this.price = price;
+        this.payment = payment;
+        this.housePrice = housePrice;
+        this.hotelPrice = hotelPrice;
+        this.propertyName = propertyName;
+        this.printName = printName;
+        this.thisTileType = thisTileType;
+    }
 }
--- Game.java
+++ Game.java
@@ -5,6 +5,47 @@ public class Game {
     CircularLinkedList tiles = new CircularLinkedList();
 
     public Game(){
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Boardwalk", "BW", BoardSpace.tileType.Blue));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Luxury Tax", "LT", BoardSpace.tileType.Utilities));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Park Place", "PP", BoardSpace.tileType.Blue));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Chance", "Ch", BoardSpace.tileType.Chance));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Short Line", "SL", BoardSpace.tileType.Railroad));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Pennsylvania", "PA", BoardSpace.tileType.Green));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Community Chest", "CC", BoardSpace.tileType.CommunityChest));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"North Carolina", "NC", BoardSpace.tileType.Green));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Pacific", "PC", BoardSpace.tileType.Green));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Go To Jail", "TJ", BoardSpace.tileType.GoToJail));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Marvin Gardens", "MG", BoardSpace.tileType.Yellow));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Water Works", "WW", BoardSpace.tileType.Utilities));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Ventnor Avenue", "Ve", BoardSpace.tileType.Yellow));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Atlantic Avenue", "AA", BoardSpace.tileType.Yellow));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Boardwalk", "BW", BoardSpace.tileType.Blue));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Boardwalk", "BW", BoardSpace.tileType.Blue));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Boardwalk", "BW", BoardSpace.tileType.Blue));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Boardwalk", "BW", BoardSpace.tileType.Blue));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Boardwalk", "BW", BoardSpace.tileType.Blue));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Boardwalk", "BW", BoardSpace.tileType.Blue));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Boardwalk", "BW", BoardSpace.tileType.Blue));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Boardwalk", "BW", BoardSpace.tileType.Blue));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Boardwalk", "BW", BoardSpace.tileType.Blue));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Boardwalk", "BW", BoardSpace.tileType.Blue));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Boardwalk", "BW", BoardSpace.tileType.Blue));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Boardwalk", "BW", BoardSpace.tileType.Blue));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Boardwalk", "BW", BoardSpace.tileType.Blue));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Boardwalk", "BW", BoardSpace.tileType.Blue));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Boardwalk", "BW", BoardSpace.tileType.Blue));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Boardwalk", "BW", BoardSpace.tileType.Blue));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Boardwalk", "BW", BoardSpace.tileType.Blue));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Boardwalk", "BW", BoardSpace.tileType.Blue));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Boardwalk", "BW", BoardSpace.tileType.Blue));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Boardwalk", "BW", BoardSpace.tileType.Blue));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Boardwalk", "BW", BoardSpace.tileType.Blue));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Boardwalk", "BW", BoardSpace.tileType.Blue));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Boardwalk", "BW", BoardSpace.tileType.Blue));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Boardwalk", "BW", BoardSpace.tileType.Blue));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Boardwalk", "BW", BoardSpace.tileType.Blue));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Boardwalk", "BW", BoardSpace.tileType.Blue));
+        tiles.insertFirst(new BoardSpace(400, 0, 0,0,"Boardwalk", "BW", BoardSpace.tileType.Blue));
 
     }
 
