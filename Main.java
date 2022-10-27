149026c1cbace0c66dfcfdf787dc1da5fecd6044
ndoughty4 <111516051+ndoughty4@users.noreply.github.com>
2022-10-27 11:11:26 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,23 +1,65 @@
 public class Main {
     public static void main(String[] args) {
 
-
-        Player bob = new Player("Bob");
-        Player joe = new Player("Joe");
-        Player jim = new Player("Jim");
-
-
         Game newGame = new Game();
-        newGame.players.insertFirst(bob);bob.setCurrentBoardSpace((BoardSpace)(newGame.tiles.first.data));
-        newGame.players.insertFirst(joe);joe.setCurrentBoardSpace((BoardSpace)(newGame.tiles.first.data));
-        newGame.players.insertFirst(jim);jim.setCurrentBoardSpace((BoardSpace)(newGame.tiles.first.data));
-        newGame.currentPlayer = (Player)(newGame.players.first.data);
-        newGame.printBoard(jim);
-        ((BoardSpace)(newGame.tiles.first.data)).addToCurrentPlayers(jim);
-        ((BoardSpace)(newGame.tiles.first.data)).addToCurrentPlayers(bob);
-        ((BoardSpace)(newGame.tiles.first.data)).printBoardSpaceAttributes();
-        newGame.diceRoll();
-        newGame.printBoard(bob);
-        bob.getCurrentBoardSpace().printBoardSpaceAttributes();
+        newGame.printListOfBoard();
+        newGame.printBoard();
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Please enter the name of your first player:");
+        Player player1 = new Player(in.nextLine());
+        newGame.players.insertFirst(player1);
+        System.out.println("Please enter the name of your second player:");
+        Player player2 = new Player(in.nextLine());
+        newGame.players.insertFirst(player2);
+        System.out.println("Would you like to add another player? (n:0, y:1)");
+        if(Integer.parseInt(in.nextLine()) == 1){
+            Player player3 = new Player(in.nextLine());
+            newGame.players.insertFirst(player3);
+
+            System.out.println("Would you like to add another player? (n:0, y:1)");
+            if(Integer.parseInt(in.nextLine()) == 1){
+                Player player4 = new Player(in.nextLine());
+                newGame.players.insertFirst(player4);
+
+                System.out.println("Would you like to add another player? (n:0, y:1)");
+                if(Integer.parseInt(in.nextLine()) == 1){
+                    Player player5 = new Player(in.nextLine());
+                    newGame.players.insertFirst(player5);
+                    if(Integer.parseInt(in.nextLine()) == 1){
+                        Player player6 = new Player(in.nextLine());
+                        newGame.players.insertFirst(player6);
+                    }
+                }
+            }
+        }
+
+        boolean gameOn = true;
+        Link currentPlayerLink = newGame.players.first;
+
+        while(gameOn){
+
+            if(((Player)(currentPlayerLink.data)).jailRolls > 0){
+                newGame.getOutOfJail((Player)(currentPlayerLink.data));
+            } else {
+
+                newGame.turn();
+                System.out.println("Would you like to propose a trade? (n:0, y:1)");
+                if(Integer.parseInt(in.nextLine())==1){
+                    newGame.trade();
+                }
+                if(!((Player)(currentPlayerLink.data)).completedSets.isEmpty()){
+                    System.out.println("Would you like to upgrade (add houses/hotels to) any of your completed sets? (n:0, y:1)");
+                    if(Integer.parseInt(in.nextLine())== 1){
+                        newGame.upgrade();
+                    }
+                }
+                
+
+            }
+            currentPlayerLink = currentPlayerLink.nextLink;
+        }
+       
     }
 }
