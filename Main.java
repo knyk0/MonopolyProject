b0c7753534f5743d39861e3ff85896d925fd6f58
ndoughty4 <111516051+ndoughty4@users.noreply.github.com>
2022-10-28 17:05:36 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -14,19 +14,21 @@ public class Main {
         newGame.players.insertFirst(player2);
         System.out.println("Would you like to add another player? (n:0, y:1)");
         if(Integer.parseInt(in.nextLine()) == 1){
+            System.out.println("Please enter the name of your third player:");
             Player player3 = new Player(in.nextLine(),newGame.tiles);
             newGame.players.insertFirst(player3);
-
             System.out.println("Would you like to add another player? (n:0, y:1)");
             if(Integer.parseInt(in.nextLine()) == 1){
+                System.out.println("Please enter the name of your fourth player:");
                 Player player4 = new Player(in.nextLine(),newGame.tiles);
                 newGame.players.insertFirst(player4);
-
                 System.out.println("Would you like to add another player? (n:0, y:1)");
                 if(Integer.parseInt(in.nextLine()) == 1){
+                    System.out.println("Please enter the name of your fifth player:");
                     Player player5 = new Player(in.nextLine(),newGame.tiles);
                     newGame.players.insertFirst(player5);
                     if(Integer.parseInt(in.nextLine()) == 1){
+                        System.out.println("Please enter the name of your sixth player:");
                         Player player6 = new Player(in.nextLine(),newGame.tiles);
                         newGame.players.insertFirst(player6);
                     }
@@ -46,7 +48,7 @@ public class Main {
                 if(newGame.currentPlayer.getJailRolls() <= 0 && newGame.currentPlayer.getMoney() > 0){
                     System.out.println("Would you like to propose a trade? (n:0, y:1)");
                     if(Integer.parseInt(in.nextLine())==1){
-                        newGame.trade();
+                        newGame.trade(newGame.currentPlayer);
                     }
                     if(!((Player)(currentPlayerLink.data)).completedSets.isEmpty()){
                         System.out.println("Would you like to upgrade (add houses/hotels to) any of your completed sets? (n:0, y:1)");
